7e11924d87e650db15c7982c698ecdc53a7d4239
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2022-10-13 21:21:05 -0700

    running test navigation for now

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,29 @@
 import { React } from 'react';
 import '../App.css';
 
-function NavBar() {
+function NavBar(props) {
     return (
-            <div className="navbar">
-                <ul className="navbar-nav">
-                    <li className="navbar_item">Home</li>
-                    <li className="navbar_item">Account</li>
-                    <li className="navbar_item">Project</li>
-                    <li className="navbar_item">Team</li>
-                </ul>
+            <div className="menu-wrap">
+                <input type="checkbox" class="toggler" />
+                <div class="hamburger"></div><div></div>
+                <div class="menu">
+                    <div>
+                        <div>
+                            <ul>
+                                <li>Home</li>
+                                <li>Account</li>
+                                <li>Project</li>
+                                <li>Team</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <header class="showcase">
+                    <div class="container showcase-inner">
+                        <h1>Milestone 3</h1>
+                        <p>jaoweifja;sldfkjsa;ldkfjas;dlfjas;ldkfja;lsdfja;slkdf</p>
+                    </div>
+                </header>
             </div>
     )
 }
