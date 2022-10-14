3b67339e79a8d01ac67c03a12a653273d7d1c778
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2022-10-13 20:23:58 -0700

    finished basic navbar section

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar.js';
+import './components/css/NavBar.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import { React } from 'react';
+import './css/NavBar.css';
+
+function NavBar() {
+    return (
+        <div>
+            <div className="navbar">
+                <ul>
+                    <li>Home</li>
+                    <li>Account</li>
+                    <li>Project</li>
+                    <li>Team</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
