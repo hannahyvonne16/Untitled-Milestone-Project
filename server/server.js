404f6bd0542f265646154967c5b319e4f2b25a41
allison ruiz <allisonnruizz@gmail.com>
2022-10-14 18:55:02 -0700

    changes to server node modules and folders

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-import express from 'express'
-const PORT = 4000;
+const express = require('express')
+const path =require('path')
+const PORT = process.env.PORT || 4000
 const app = express();
 
 app.get('/', (req, res) => {
