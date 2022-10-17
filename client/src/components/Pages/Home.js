73e41116090bdd1db09fa03b32f1e524aba03ad9
AngelTheCreator <angel_guevara94@hotmail.com>
2022-10-16 21:45:45 -0700

    added ReactMap GL and the viewport. Page renders but it demands a token in order to show the map

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,11 +1,17 @@
-import { useMemo } from "react";
-import { ReactMapGL } from 'react-map-gl'
+import { React, useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
  function Home() {
+  const [viewport, setViewport] = useState({
+    latitude: 40.741895,
+    longitude: -73.989308,
+    zoom: 10,
+    width: '100vw',
+    height: '100vh'
+  })
+
   return <div>
-    <ReactMapGL>
-      
-    </ReactMapGL>
+    <ReactMapGL {...viewport}>Map Here</ReactMapGL>
   </div>
 }
 
