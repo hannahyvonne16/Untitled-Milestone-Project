cb899715b6d399ded1eab0aa5abbcb0e354386d8
AngelTheCreator <angel_guevara94@hotmail.com>
2022-10-17 20:48:18 -0700

    started the project page and changed mongo

--- client/src/components/Pages/Project.js
+++ client/src/components/Pages/Project.js
@@ -1,6 +1,29 @@
 import { React } from 'react';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 function Project() {
+    const projects = [{
+        title: 'Miro',
+        description: 'SWOT Analysis',
+        imgUrl: ''
+    },
+    {
+        title: 'Figma',
+        description: 'Brainstorming',
+        imgUrl: ''
+    },
+    {
+        title: 'Figma 2',
+        description: 'Wireframe',
+        imgUrl: ''
+    },
+    {
+        title: 'Miro',
+        description: 'SWOT Analysis',
+        imgUrl: ''
+    },
+    ]
     return (
         <>
             <h1>Project</h1>
