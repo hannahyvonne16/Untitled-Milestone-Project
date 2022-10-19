9772a915eb270011ff9d59990cc65dc845e0a8a0
Hannah Lee <hannahyvonne16@gmail.com>
2022-10-18 22:45:43 -0700

    more locations

---
+++ server/controllers/toilets_controller.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const toilets = require('../models/toilets')
+const db= require("../models")
+
+router.get('/test', (req,res) => {
+    db.Toilets.find()
+    .then((toilets) => {
+        console.log("sending")
+        res.send(toilets)
+    })
+})
+
+module.exports = router
