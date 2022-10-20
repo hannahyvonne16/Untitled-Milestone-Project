f19cd61509e635a1c163ec89f3257e49a50f79f8
Hannah Lee <hannahyvonne16@gmail.com>
2022-10-20 14:35:44 -0700

    adding more CSULB locations

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -73,12 +73,95 @@ const data = [
 		"coordinates": [-118.10920693119864,33.784329852984335],
 	},
 	{
-		"location": "CSULB Studen Recreation and Wellness Center",
+		"location": "CSULB Student Recreation and Wellness Center",
 		"city": "Long Beach",
 		"state": "California",
 		"coordinates": [-118.1093394060906,33.7851575997318],
 	},
-
+	{
+		"location": "CSULB Mailing Services",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.10924553876404,33.78334944943123],
+	},
+	{
+		"location": "CSULB Human Services and Design",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.1095878784254,33.78276202181796],
+	},
+	{
+		"location": "CSULB Design Center",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.10956027038979,33.782009374305275],
+	},
+	{
+		"location": "CSULB Central Plant",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.1122212923896,33.781683462500006],
+	},
+	{
+		"location": "CSULB Kinesiology Building",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11223986478913,33.78267526328235],
+	},
+	{
+		"location": "CSULB Psychology Building",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11428494247951,33.779257184449015],
+	},
+	{
+		"location": "CSULB Department of Asian and Asian American Studies",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11369413048553,33.779192931681585],
+	},
+	{
+		"location": "CSULB College of Liberal Arts",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.1140969779595,33.777779181811226],
+	},
+	{
+		"location": "CSULB Peterson Hall 1",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.1126503168153,33.77880264776819],
+	},
+	{
+		"location": "CSULB Fine Arts Building 3",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11239632287395,33.77782966696578],
+	},
+	{
+		"location": "CSULB Fine Arts Building 1",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11263927360045,33.777219253556524],
+	},
+	{
+		"location": "CSULB Liberal Arts 1",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11487499275916,33.77767239333733],
+	},
+	{
+		"location": "CSULB Education Psychology Clinc",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11414097194172,33.77575763688348],
+	},
+	{
+		"location": "CSULB Fine Arts Building 4",
+		"city": "Long Beach",
+		"state": "California",
+		"coordinates": [-118.11263492499057,33.77835927331352],
+	},
 ]
 
 class Home extends React.Component{
