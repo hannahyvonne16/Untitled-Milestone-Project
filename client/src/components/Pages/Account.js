43fa9b4edade18289668a46d237da2d63c66a739
Sgeary88 <97266345+Sgeary88@users.noreply.github.com>
2022-10-20 19:23:25 -0700

    delete and re-cloned, filled log in page

--- client/src/components/Pages/Account.js
+++ client/src/components/Pages/Account.js
@@ -1,11 +1,76 @@
-import { React } from 'react';
+import React, { Component } from 'react';
 
-function Account() {
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        email: '',
+        password: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+    console.log(email, password);
+    fetch('http://localhost:4000/login-user', {
+      method: 'POST',
+      crossDomain: true,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, 'userRegister');
+        if (data.status === 'ok') {
+          alert('login successful');
+          window.localStorage.setItem('token', data.data);
+          window.location.href = './userDetails';
+        }
+      });
+  }
+  render() {
     return (
-        <>
-            <h1>Account</h1>
-        </>
-    )
+      <form onSubmit={this.handleSubmit}>
+        <h3>Sign In</h3>
+
+        <div>
+          <label>Email address</label>
+          <input
+            type='email'
+            className='form-control'
+            placeholder='Enter email'
+            onChange={(e) => this.setState({ email: e.target.value })}
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input
+            type='password'
+            className='form-control'
+            placeholder='Enter password'
+            onChange={(e) => this.setState({ password: e.target.value })}
+          />
+        </div>
+
+        <div>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </div>
+        <p>
+        <a href='/sign-up'>Sign up</a>
+        </p>
+      </form>
+    );
+  }
 }
 
-export default Account
